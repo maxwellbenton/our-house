5c0439c80fec5f4eab6df42b0226af5a2b3f8f98
github username <github email address>
2017-09-14 15:41:58 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :sections, only: [:new, :create]
   resources :users, only: [:index, :show]
 
+
   root 'house_images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
